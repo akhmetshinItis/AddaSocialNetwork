f390a0542fdf904de8ec7a64e7c5521c3af4442e
akhmetshinItis <akhmetshin.itis@mail.ru>
2025-06-12 21:10:11 +0300

    форма регистрации пользователя из админки

--- Semestrovka2/Web/Areas/Admin/Controllers/UsersController.cs
+++ Semestrovka2/Web/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using Contracts.Requests.UserRequests.RegisterUser;
 using Core.Requests.AdminRequests.UserRequests;
+using Core.Requests.UserRequests.RegisterUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,5 +21,38 @@ public class UsersController : Controller
     public async Task<IActionResult> Index()
     {
         return View(await _mediator.Send(new GetAllUsersQuery()));
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create([FromForm] RegisterUserRequest request)
+    {
+        var command = new RegisterUserCommand
+        {
+            Email = request.Email,
+            Password = request.Password,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Gender = request.Gender,
+            Age = request.Age,
+            Country = request.Country
+        };
+
+        var result = await _mediator.Send(command);
+
+        if (!result.Result.Succeeded)
+        {
+            foreach (var error in result.Result.Errors)
+                ModelState.AddModelError("", error.ToString() ?? string.Empty);
+            return View(request);
+        }
+
+        TempData["SuccessMessage"] = "Пользователь успешно создан!";
+        return RedirectToAction("CreateUser");
     }
 } 
